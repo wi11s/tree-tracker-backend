2fda0664d1935f1fb9fe2f3d86326922bf0321d1
William <wms274@nyu.edu>
2023-03-07 18:39:37 -0500

    test data and fixes seed data

--- app/models/tree_type.rb
+++ app/models/tree_type.rb
@@ -2,6 +2,6 @@ class TreeType < ApplicationRecord
     validates :common_name, uniqueness: true
     validates :common_name, :image, :frequency, presence: true
 
-    has_many :join_types
+    has_many :join_types, dependent: :destroy
     has_many :users, through: :join_types
 end
